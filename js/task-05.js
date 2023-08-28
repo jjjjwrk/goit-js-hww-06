40672f09e4a9fc7bcbd1cd01518edbc39f69e973
jjjjwrk <130352649+jjjjwrk@users.noreply.github.com>
2023-08-28 12:16:30 -0700

    update

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,7 @@
 const inputForm = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 inputForm.addEventListener('input', event => {
-  const inputValue = event.currentTarget.value;
-  output.textContent = inputValue.trim();
+  const inputValue = event.currentTarget.value.trim();
+  output.textContent = inputValue;
   output.textContent = inputValue === '' ? 'Anonymous' : inputValue.trim();
 });
--- js/task-08.js
+++ js/task-08.js
@@ -8,16 +8,16 @@ function submitForm(event) {
 
     const email = formElements.email.value;
     const password = formElements.password.value;
-
     const formData = {
         email,
         password
     };
-    console.log(formData);
 
     if(email === '' || password === ''){
         alert('Заполни все поля');
-    };
+    } else {
+        console.log(formData);
+    }
     form.reset();
 }
 
